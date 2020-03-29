ac2ff77a5b568bd758c1d1c19aecf54a671d59bb
Ibrahim Kurhan <Ibrahim.Kurhan@Nology.local>
2020-03-29 19:03:01 +0100

    hamming distance solved

--- app.js
+++ app.js
@@ -172,3 +172,42 @@ console.log(triangel.check());
 
 
 // ================================================================================================================
+
+// Calculate the Hamming Distance between two DNA strands.
+
+// Your body is made up of cells that contain DNA. Those cells regularly wear out and need replacing, 
+// which they achieve by dividing into daughter cells. 
+// In fact, the average human body experiences about 10 quadrillion cell divisions in a lifetime!
+
+// When cells divide, their DNA replicates too. 
+// Sometimes during this process mistakes happen and single pieces of DNA get encoded with the incorrect information.
+// If we compare two strands of DNA and count the differences between them we can see how many mistakes occurred. 
+// This is known as the "Hamming Distance".
+
+// We read DNA using the letters C,A,G and T. Two strands might look like this:
+
+// GAGCCTACTAACGGGAT
+// CATCGTAATGACGGCCT
+// ^ ^ ^  ^ ^    ^^
+// They have 7 differences, and therefore the Hamming Distance is 7.
+
+// The Hamming Distance is useful for lots of things in science, not just biology, 
+// so it's a nice phrase to be familiar with :)
+
+const first = "GAGCCTACTAAGGG";
+const second = "AGACTCATAAGGCA"
+const arr1 = first.split("");
+const arr2 = second.split("");
+const dna = (arr1, arr2) => {
+    let distance = 0;
+    for (let i = 0; i < first.length; i++) {
+        if (first.length !== second.length) {
+            return false;
+        } if (arr1[i] !== arr2[i]) {
+            distance++
+        }
+    }
+    return distance;
+}
+
+console.log(dna(arr1, arr2));
