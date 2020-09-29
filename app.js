3067d45dd4768fad62be9b4c656fe6654415f998
Ibrahim Kurhan <ikurhan@outlook.com>
2020-09-29 13:53:44 +0100

    tech test added

--- app.js
+++ app.js
@@ -658,3 +658,41 @@ const alerigi = (num) => {
 };
 console.log(alerigi(32));
 // ===========================================================================================
+// D&D Character =============================================================================================
+// For a game of [Dungeons & Dragons][DND], each player starts
+// by generating a character they can play with.
+// This character has, among other things, six abilities;
+// strength, dexterity, constitution, intelligence, wisdom and charisma.
+// These six abilities have scores that are determined randomly.
+// You do this by rolling four 6-sided dice and record the sum of the largest three dice.
+// You do this six times, once for each ability.
+// Your character's initial hitpoints are 10 + your character's constitution modifier.
+// You find your character's constitution modifier by subtracting 10 from your character's constitution,
+// divide by 2 and round down.
+// Write a random character generator that follows the rules above.
+
+let dice1 = Math.floor(Math.random() * 6) + 1;
+let dice2 = Math.floor(Math.random() * 6) + 1;
+let dice3 = Math.floor(Math.random() * 6) + 1;
+let dice4 = Math.floor(Math.random() * 6) + 1;
+
+let allDice = [dice1, dice2, dice3, dice4];
+
+let TheBiggestThree = allDice.sort().splice(1);
+console.log(TheBiggestThree);
+
+let total = TheBiggestThree.reduce((a, b) => a + b, 0);
+console.log(total);
+
+const constitutionModifier = (total) => {
+  return Math.floor((total - 10) / 2);
+};
+let newParamater = constitutionModifier(total);
+console.log(newParamater);
+
+const HitPoint = (newParamater) => {
+  return newParamater + 10;
+};
+console.log(HitPoint(newParamater));
+
+// =======================================================================================================
