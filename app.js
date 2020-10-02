a0bd3aa994f7f46b3b095a634d9992c3f3611b82
Ibrahim Kurhan <ikurhan@outlook.com>
2020-10-02 14:12:58 +0100

    tech test added

--- app.js
+++ app.js
@@ -696,3 +696,73 @@ const HitPoint = (newParamater) => {
 console.log(HitPoint(newParamater));
 
 // =======================================================================================================
+const type = document.getElementsByClassName("typeinput");
+
+const letters = {
+  a: ".-",
+  b: "-...",
+  c: "-.-.",
+  d: "-..",
+  e: ".",
+  f: "..-.",
+  g: "--.",
+  h: "....",
+  i: "..",
+  j: ".---",
+  k: "-.-",
+  l: ".-..",
+  m: "--",
+  n: "-.",
+  o: "---",
+  p: ".--.",
+  q: "--.-",
+  r: ".-.",
+  s: "...",
+  t: "-",
+  u: "..-",
+  v: "...-",
+  w: ".--",
+  x: "-..-",
+  y: "-.--",
+  z: "--..",
+  1: ".----",
+  2: "..---",
+  3: "...--",
+  4: "....-",
+  5: ".....",
+  6: "-....",
+  7: "--...",
+  8: "---..",
+  9: "----.",
+  0: "-----",
+  ".": ".-.-.-",
+  ",": "--..--",
+  "?": "..--..",
+  "'": ".----.",
+  "!": "-.-.--",
+  "/": "-..-.",
+  "(": "-.--.",
+  ")": "-.--.-",
+  "&": ".-...",
+  ":": "---...",
+  ";": "-.-.-.",
+  "=": "-...-",
+  "+": ".-.-.",
+  "-": "-....-",
+  _: "..--.-",
+  '"': ".-..-.",
+  $: "...-..-",
+  "@": ".--.-.",
+  " ": " ",
+};
+const convert = (letter) =>
+  Object.values(letters).find((value) => value === letters[letter]);
+
+const mapToMorseCode = () => {
+  const userInput = type[0].value.toLowerCase();
+  const letterArray = userInput.split("");
+  const convertedLetterArray = letterArray.map(convert);
+  const resulted = convertedLetterArray.join("");
+  document.getElementById("result").innerHTML = resulted;
+};
+document.getElementById("convert").addEventListener("click", mapToMorseCode);
